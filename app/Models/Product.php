a7276b1b16a77c8f5801f994481534cc28e8bb3e
sharansherpunja <sharansherpunja@my.unt.edu>
2020-11-07 23:14:29 -0600

    created product tag relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = ['name' , 'description' , 'cost' , 'tag_id'];
+
+    public function tag()
+    {
+        return $this->belongsTo(Tag::class);
+    }
 }
